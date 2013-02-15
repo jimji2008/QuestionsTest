708167536326f21c418c906d11d8c614c3dffdee
jim <jimji2008@gmail.com>
2013-02-15 21:10:49 +0800

    update candies

--- Candies/Program2.cs
+++ Candies/Program2.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace Candies
 {
-	class Program
+	class Program2
 	{
+		static Child[] children = null;
 		static void Main(string[] args)
 		{
 			int n = int.Parse(Console.ReadLine());
@@ -15,28 +16,116 @@ namespace Candies
 				ratings[i] = int.Parse(Console.ReadLine());
 			}
 
-			int[] candies = new int[n];
-			candies[0] = 1;
-			candies[n-1]=1;
+			children = new Child[n];
+			//children[0] = new Child() { candiesNum = 1 };
+			//children[n - 1] = new Child() { candiesNum = 1 };
+
+			// prepare
 			for (int i = 0; i < n; i++)
 			{
-				ratings[i] = int.Parse(Console.ReadLine());
+				children[i] = new Child();
+				if (i == 0)
+				{
+					if (ratings[i] > ratings[i + 1])
+					{
+						children[i].RightInx = i + 1;
+						children[i].greaterThanRight = true;
+					}
+					else
+					{
+						children[i].candiesNum = 1;
+					}
+				}
+				else if (i == n - 1)
+				{
+					if (ratings[i] > ratings[i - 1])
+					{
+						children[i].LeftInx = i - 1;
+						children[i].greaterThanLeft = true;
+					}
+					else
+					{
+						children[i].candiesNum = 1;
+					}
+				}
+				else
+				{
+					if (ratings[i] <= ratings[i - 1] && ratings[i] <= ratings[i + 1])
+					{
+						children[i].candiesNum = 1;
+					}
+					if (ratings[i] > ratings[i - 1] && ratings[i] > ratings[i + 1])
+					{
+						children[i].LeftInx = i - 1;
+						children[i].RightInx = i + 1;
+						children[i].greaterThanLeft = true;
+						children[i].greaterThanRight = true;
+					}
+					else if (ratings[i] > ratings[i - 1])
+					{
+						children[i].LeftInx = i - 1;
+						children[i].greaterThanLeft = true;
+					}
+					else if (ratings[i] > ratings[i + 1])
+					{
+						children[i].RightInx = i + 1;
+						children[i].greaterThanRight = true;
+					}
+				}
 			}
-			
-			
+
+			// count
+			int num = 0;
+			for (int i = 0; i < n; i++)
+			{
+				//Console.Write(children[i].ComputeCandiesNum()+",");
+				num += children[i].ComputeCandiesNum();
+			}
+
 			Console.Write(num);
 		}
 
 		class Child
 		{
-			public bool HaveCandies()
+			//public bool HaveCandies()
+			//{
+			//    return candiesNum != int.MaxValue;
+			//}
+
+			public int candiesNum = int.MaxValue;
+
+			public bool greaterThanLeft = false;
+			public bool greaterThanRight = false;
+			public int LeftInx = int.MaxValue;
+			public int RightInx = int.MaxValue;
+			public int ComputeCandiesNum()
 			{
-				return candiesNum!=int.MaxValue;
+				if (candiesNum != int.MaxValue)
+					return candiesNum;
+
+				if (greaterThanLeft && greaterThanRight)
+				{
+					candiesNum = Math.Max(children[LeftInx].ComputeCandiesNum() + 1,
+						children[RightInx].ComputeCandiesNum() + 1);
+					return candiesNum;
+				}
+				else if (greaterThanLeft)
+				{
+					candiesNum = children[LeftInx].ComputeCandiesNum() + 1;
+					return candiesNum;
+				}
+				else if (greaterThanRight)
+				{
+					candiesNum = children[RightInx].ComputeCandiesNum() + 1;
+					return candiesNum;
+				}
+				else 
+				{
+					candiesNum = 1;
+					return candiesNum;
+				}
+
 			}
-
-			public int candiesNum;
-
-			public Func<int> Condition;
 		}
 	}
 }
