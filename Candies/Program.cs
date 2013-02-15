1393143b33df0ce2ce189c676ed685cec043c86c
jim <jimji2008@gmail.com>
2013-02-15 19:52:50 +0800

    add candies

---
+++ Candies/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Candies
+{
+	class Program
+	{
+		static void Mainx(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int[] ratings = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				ratings[i] = int.Parse(Console.ReadLine());
+			}
+
+			int num = 0;
+			int lastCandiesNum = 0;
+			for (int i = 0; i < n; i++)
+			{
+				int candiesNum = 0;
+				if (i == 0)
+				{
+					candiesNum = 1;
+				}
+				else
+				{
+					if (ratings[i] > ratings[i - 1])
+					{
+						candiesNum = lastCandiesNum + 1;
+					}
+					else
+					{
+						candiesNum = 1;
+					}
+				}
+				num += candiesNum;
+				lastCandiesNum = candiesNum;
+			}
+
+			Console.Write(num);
+		}
+	}
+}
---
+++ Candies/Program2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Candies
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int[] ratings = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				ratings[i] = int.Parse(Console.ReadLine());
+			}
+
+			int[] candies = new int[n];
+			candies[0] = 1;
+			candies[n-1]=1;
+			for (int i = 0; i < n; i++)
+			{
+				ratings[i] = int.Parse(Console.ReadLine());
+			}
+			
+			
+			Console.Write(num);
+		}
+
+		class Child
+		{
+			public bool HaveCandies()
+			{
+				return candiesNum!=int.MaxValue;
+			}
+
+			public int candiesNum;
+
+			public Func<int> Condition;
+		}
+	}
+}
