ce1af7217814af617c81fbc19137e6508b5597ac
jim <jimji2008@gmail.com>
2013-02-17 22:53:14 +0800

    add fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Mainx(string[] args)
+		{
+			int x = 0;
+			while (x++ < 100)
+			{
+				bool b = false;
+				if (x % 3 == 0)
+				{
+					Console.Write("Fizz");
+					b = true;
+				} 
+				if (x % 5 == 0)
+				{
+					Console.Write("Buzz");
+					b = true;
+				}
+				if (!b)
+					Console.Write(x.ToString());
+				Console.WriteLine();
+			}
+
+			//Console.WriteLine(new WebClient().DownloadString(@"http://cdn.hackerrank.com/fizzbuzz.txt"));
+		}
+		//static bool f(string s)
+		//{
+		//    Console.Write(s);
+		//    return true;
+		//}
+	}
+}
---
+++ FizzBuzz/Program2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+namespace FizzBuzz
+{
+	class Program2
+	{
+		static void Main(string[] args)
+		{
+			//Enumerable.Range(1, 100).ToList().ForEach(x =>
+			//{
+			//    Console.WriteLine(x % 3 == 0 && x % 5 == 0 ? "FizzBuzz" : x % 3 == 0 ?
+			//        "Fizz" :
+			//        (x % 5 == 0 ? "Buzz" : x + ""));
+			//});
+
+			for(int i=0;i<100;f(++i));
+
+		//    Func<string,bool> f = s =>
+		//{
+		//    Console.Write(s);
+		//    return true;
+		//};
+		//    int x = 1;
+		//    while(x++<101)
+
+		//    //Enumerable.Range(1, 100).ForEach(x =>
+		//    {
+		//        //var a = x%3==0&&f("Fizz")||(x%5==0?f("buzz"):f(x+""))||f("\n");
+		//        var a = x % 3 == 0 && f("Fizz");
+		//        a = x % 5 == 0 && f("Buzz") || a;
+		//        a = a || f(x + "");
+		//        f("\n");	
+		//    }//);
+
+		//    //from x in Enumerable.Range(1, 100)
+			//    select x%3&&f("Fizz")||x%5&&f("Fizz")
+
+			//var q=Enumerable.Range(1, 100);
+			//foreach (int x in q)
+			//{
+			//    if
+			//}
+
+			//Console.WriteLine(new WebClient().DownloadString(@"http://cdn.hackerrank.com/fizzbuzz.txt"));
+		}
+		static void f(int i)
+		{
+			string s=((i % 3 != 0 ? "" : "Fizz") + (i % 5 != 0 ? "" : "Buzz"));
+			if (s == "") s = i + "";
+			Console.WriteLine(s);
+		}
+	}
+}
